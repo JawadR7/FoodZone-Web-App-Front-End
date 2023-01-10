76b90b6862bc801a035653edb99151a8170fdfdd
JawadR7 <41636087+JawadR7@users.noreply.github.com>
2023-01-09 19:48:49 -0500

    Update App.js

--- foodzone-web-app/src/components/App.js
+++ foodzone-web-app/src/components/App.js
@@ -25,19 +25,12 @@ function App() {
    * <Front /> for first page
    * <RestList /> for restaurant list
    * <RestProf /> for restaurant profile
+   Uncomment proper return line below to see corresponding page
    */
-  return <RestList />;
-  /*
-  return (
-    <SafeAreaView style={styles.container}>
-      <div className="App">
-        <NavBar />
-        <Landing />
-      </div>
-    </SafeAreaView>
-  );
-  */
-}
+  
+  return <Front />;
+  //return <RestList />;
+  //return <RestProf />;
 
 const styles = StyleSheet.create({
   container: {
@@ -74,70 +67,3 @@ const styles = StyleSheet.create({
 });
 
 export default App;
-
-//UIView
-/*
-const App = () => {
-  return (
-    <SafeAreaView style={styles.container}>
-      <ImageBackground
-        //Background image on homepage
-        style={styles.logo}
-        //source={require('./assets/foodzone_logo.png')}
-        resizeMode="contain"
-      >
-        <Text style={styles.text}>FOODZONE</Text>
-
-        <TouchableOpacity
-          //Order Button on homepage
-          style={styles.button}
-          onPress={onPressHandler}
-        >
-          <Text style={styles.text}>ORDER</Text>
-        </TouchableOpacity>
-      </ImageBackground>
-    </SafeAreaView>
-  );
-};
-//for button press
-const onPressHandler = () => {};
-const separator = () => {
-  <View style={styles.container}></View>;
-};
-//For design of texts,button, etc.
-const styles = StyleSheet.create({
-  container: {
-    flex: 1
-  },
-  text: {
-    color: "green",
-    fontSize: 25,
-    //lineHeight: 74,
-    fontStyle: "italic",
-    fontWeight: "bold",
-    textAlign: "center",
-    textAlignVertical: -558
-  },
-  logo: {
-    flex: 1,
-    justifyContent: "center"
-  },
-  button: {
-    backgroundColor: "yellow",
-    width: 100,
-    height: 50,
-    alignItems: "center"
-  },
-  separator: {
-    marginVertical: 8,
-    borderBottomColor: "#737373",
-    borderBottomWidth: StyleSheet.hairlineWidth
-  },
-  title: {
-    textAlign: "center",
-    marginVertical: 8
-  }
-});
-
-export default App;
-*/
